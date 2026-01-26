e90c6488df80611e8a646fe93e3bfe23be999136
shuya-ubagai <T125019@ed.sus.ac.jp>
2026-01-26 11:07:29 +0900

    dataframeに両県の情報を表示

--- app.py
+++ app.py
@@ -10,7 +10,7 @@ with st.sidebar:
     st.header("条件選択")
     region = st.multiselect("地域の選択",
                            df_niigata["(J315-02-2-001)新潟県地域"].unique())
-    keitai = st.selectbox("経営体系の選択",
+    keitai = st.radio("経営体系の選択",
                          df_niigata["(J301-02-1-001)農林業経営体数"].unique())
     
 df_n = df_niigata[
@@ -19,12 +19,13 @@ df_n = df_niigata[
 ]  # 授業と違い、2つ同時に定義
 
 df_h = df_nagano[
-    (df_nagano["(J320-02-2-001)長野県地域"].isin(region)) &
     (df_nagano["(J301-02-1-001)農林業経営体数"] == keitai)
     ]
 
+st.subheader("新潟県")
 df_n.set_index("(J315-02-2-001)新潟県地域", inplace=True)
 st.dataframe(df_n, width=800, height=200)
 
+st.subheader("長野県")
 df_h.set_index("(J320-02-2-001)長野県地域", inplace=True)
 st.dataframe(df_h, width=800, height=200)
